d274cd2ee406be01d499dcd7526d273363efda6e
Corailla20 <petite.corailla@hotmail.fr>
2017-05-19 16:30:23 +0200

    Add isOnline() function

--- java/com/example/isit_mp3c/projet/MainActivity.java
+++ java/com/example/isit_mp3c/projet/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.isit_mp3c.projet;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.isit_mp3c.projet.camera.CameraActivity;
 import com.example.isit_mp3c.projet.exportdb.ExportDBActivity;
@@ -31,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if(isOnline())
+            Toast.makeText(this, "Connecté", Toast.LENGTH_LONG).show();
+            //send data
+        else
+            Toast.makeText(this, "Non connecté", Toast.LENGTH_LONG).show();
 
 /*        FloatingActionButton buttonCamera = (FloatingActionButton) findViewById(R.id.buttonCamera);
         buttonCamera.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
+
 
         addPatientBtn = (Button) findViewById(R.id.add_patient_button);
         addPatientBtn.setOnClickListener(new View.OnClickListener() {
@@ -166,4 +176,14 @@ public class MainActivity extends AppCompatActivity {
         alertDialog = builder.create();
         alertDialog.show();
     }
+
+    private boolean isOnline()
+    {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
+
+
